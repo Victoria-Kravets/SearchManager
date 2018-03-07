cb37961cc62798183de5097a57811b09f74ce42b
Victoria Kravets <victoriakravets93@gmail.com>
2018-03-07 13:34:55 +0300

    Initial Commit

---
+++ SearchManagerTests/SearchManagerTests.swift
@@ -0,0 +1,36 @@
+//
+//  SearchManagerTests.swift
+//  SearchManagerTests
+//
+//  Created by Admin on 07.03.2018.
+//  Copyright © 2018 Victoria Kravets. All rights reserved.
+//
+
+import XCTest
+@testable import SearchManager
+
+class SearchManagerTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
